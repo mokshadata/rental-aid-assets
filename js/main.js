f9f899ad910c16d0486d81bf63948059940ecaa9
Amanda Shih <pandafulmanda@gmail.com>
2020-05-05 21:08:23 -0500

    update ami table styling

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function addHeadersToAMITables(el) {
   var amiHeading = '80% AMI'
   var numHousefholdHeading = '# household persons'
 
-  var headerHTML = '<div class="list-grid-link no-padding w-dyn-item" style="opacity: 1;"><div class="w-row smaller-text"><div class="w-col"><div>' + 
+  var headerHTML = '<div class="list-grid-link w-dyn-item" style="opacity: 1;"><div class="w-row smaller-text"><div class="w-col"><div>' + 
     numHousefholdHeading + '</div></div><div class="w-col"><div>' +
     amiHeading + '</div></div></div></div>'
   el.innerHTML = headerHTML + el.innerHTML
