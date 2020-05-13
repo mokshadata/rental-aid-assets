5b00cb747b19236269f8bfdc166b4e44fd1d610a
Amanda Shih <pandafulmanda@gmail.com>
2020-05-13 02:56:32 -0500

    set up lang and iife

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // JS is as IE compat as possible.  Assumes jquery is loaded.
+(function() {
 
 function addHeadersToAMITables(el) {
   var annualHeading = 'Annual income'
@@ -178,33 +179,6 @@ function setupQueryFills() {
   }
 }
 
-function setupLangFromQuery() {
-  var parameters = new URLSearchParams(location.search)
-
-  if (parameters.get('language')) {
-    var language = parameters.get('language')
-
-    if  (document.querySelector('#' + language + '.boxed')) {
-      var messageEls = document.querySelectorAll('.thank-you-message')
-      Array.prototype.forEach.call(messageEls, function setupLang(el) {
-        if (el.id === language) {
-          el.classList.remove('hide')
-        } else if (!el.classList.contains('hide')) {
-          el.classList.add('hide')
-        }
-      })
-    } else {
-      var translator = document.querySelector('#google_translate_top select')
-
-      if (translator) {
-        translator.value = language.replace('_', '-')
-        translator.dispatchEvent(new Event('change'))
-      }
-    }
-  }
-}
-
-
 function handleActivators(targetTime, distInWords, timeRemaining) {
   return function(el) {
     if (el.parentElement.tagName === 'A') {
@@ -323,3 +297,32 @@ function setupPage() {
 }
 
 setupPage()
+
+})()
+
+
+function setupLangFromQuery() {
+  var parameters = new URLSearchParams(location.search)
+
+  if (parameters.get('language')) {
+    var language = parameters.get('language')
+
+    if  (document.querySelector('#' + language + '.boxed')) {
+      var messageEls = document.querySelectorAll('.thank-you-message')
+      Array.prototype.forEach.call(messageEls, function setupLang(el) {
+        if (el.id === language) {
+          el.classList.remove('hide')
+        } else if (!el.classList.contains('hide')) {
+          el.classList.add('hide')
+        }
+      })
+    } else {
+      var translator = document.querySelector('#google_translate_top select')
+
+      if (translator) {
+        translator.value = language.replace('_', '-')
+        translator.dispatchEvent(new Event('change'))
+      }
+    }
+  }
+}
